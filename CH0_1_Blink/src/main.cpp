eeacd6558797d9976b9621399b8e65afb37e45cb
JasonDPeterson <jason.peterson@minnesota.edu>
2024-11-08 18:07:31 -0600

    Creation

---
+++ CH0_1_Blink/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+int PIN_LED = 2;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+// Turn on #2 light, wait 1 second, turn off #2 light, wait 1 second.
+void loop() {
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(PIN_LED, LOW);
+  delay(1000);
+}
